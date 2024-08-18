b942501a8f715d00e6a81058a02cd0b460f10166
MarioYoussef <marioyoussef08@gmail.com>
2024-08-18 17:20:33 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:task_manager_sqlflite/features/splash_view_feature/presentation/views/splash_view.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        useMaterial3: true,
+      ),
+      home: const SplashView(),
+    );
+  }
+}
