e45b5f2d85fc0f4a550c724dcfae5e12cea77f5f
MarioYoussef <marioyoussef08@gmail.com>
2024-08-24 13:58:05 +0300

    trigger local notification cubit

--- lib/features/home_view_feature/presentation/views/home_view.dart
+++ lib/features/home_view_feature/presentation/views/home_view.dart
@@ -1,15 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:task_manager_sqlflite/core/notification_service/notification_service.dart';
 import 'package:task_manager_sqlflite/features/home_view_feature/presentation/views/widgets/custom_home_app_bar.dart';
 import 'package:task_manager_sqlflite/features/home_view_feature/presentation/views/widgets/home_view_body.dart';
+import 'package:task_manager_sqlflite/features/home_view_feature/presentation/views_models/notification_cubit/notification_cubit.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: CustomHomeAppBar(),
-      body: HomeViewBody(),
+    return BlocProvider(
+      create: (context) =>
+          NotificationCubit(NotificationService())..createNotification(context),
+      child: const Scaffold(
+        appBar: CustomHomeAppBar(),
+        body: HomeViewBody(),
+      ),
     );
   }
 }
--- lib/features/home_view_feature/presentation/views/widgets/custom_home_app_bar.dart
+++ lib/features/home_view_feature/presentation/views/widgets/custom_home_app_bar.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:task_manager_sqlflite/features/home_view_feature/presentation/views_models/dark_mode_cubit/dark_mode_cubit.dart';
+import 'package:task_manager_sqlflite/features/home_view_feature/presentation/views_models/notification_cubit/notification_cubit.dart';
 
 class CustomHomeAppBar extends StatelessWidget implements PreferredSizeWidget {
   const CustomHomeAppBar({
@@ -11,8 +13,12 @@ class CustomHomeAppBar extends StatelessWidget implements PreferredSizeWidget {
     return AppBar(
       leading: IconButton(
         icon: const Icon(Icons.nightlight_round),
-        onPressed: () async =>
-            await DarkModeCubit.get(context).changeThemeMode(),
+        onPressed: () async {
+          BlocProvider.of<NotificationCubit>(context).displayNotification(
+              title: "Theme changed",
+              body: !DarkModeCubit.get(context).isDark ? "Dark" : "Light");
+          await DarkModeCubit.get(context).changeThemeMode();
+        },
       ),
       actions: [
         IconButton(
